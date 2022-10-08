11f7f53382a92df3b2727bc0b2d0bff25639e7bb
Elliott Arnold <elliottarnold@Elliotts-Air.attlocal.net>
2022-10-08 16:26:43 -0500

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,118 @@
+// import {
+//   AppstoreOutlined,
+//   BarChartOutlined,
+//   CloudOutlined,
+//   ShopOutlined,
+//   TeamOutlined,
+//   UploadOutlined,
+//   UserOutlined,
+//   VideoCameraOutlined,
+// } from '@ant-design/icons';
+
+import { FaPlay ,FaRegHandPeace,FaInfinity, FaVolumeMute, FaVolumeUp,FaPause} from 'react-icons/fa';
+import { Layout, Menu } from 'antd';
+import React, { useRef, useState } from 'react';
+import './App.css'
+
+
+const { Header, Content, Footer, Sider } = Layout;
+// const actions = ['play','pause']
+// const color = [ 'green','red']
+
+
+
+// const items = [
+//   FaPlay,FaPause,
+ 
+// ].map((icon, index) => ({
+//   key: String(index + 1),
+//   icon: React.createElement(icon, {className: `button-${actions[index]}`, color: color[index]}),
+//   label: actions[index]
+// }));
+
+function App() {
+  const videoRef = useRef(null)
+  const [sound,setSound] = useState(false)
+  const [loop,setLoop] = useState(false)
+
+  const playVideo = () => {
+    videoRef.current.play()
+  }
+  const pauseVideo = () => {
+    videoRef.current.pause()
+  }
+
+
+  return (
+  
+  <Layout hasSider>
+    <Sider
+      style={{
+        overflow: 'auto',
+        height: '100vh',
+        position: 'fixed',
+        left: 0,
+        top: 0,
+        bottom: 0,
+        
+      }}
+    >
+      {/* <div className="logo" > 
+      <FaRegHandPeace color='silver' /> 
+      </div> */}
+
+        <Menu   theme="dark">
+            <Menu.Item><FaPlay onClick={playVideo} className='button-play'/></Menu.Item>
+            <Menu.Item><FaPause onClick={pauseVideo} className='button-pause'/></Menu.Item>
+            <Menu.Item><FaVolumeUp onClick={() => setSound(false)} className='button-volup'/></Menu.Item>
+            <Menu.Item><FaVolumeMute onClick={() => setSound(true)} className='button-voldown'/></Menu.Item>
+            <Menu.Item><FaInfinity onClick={() => setLoop(!loop)} className='button-voldown'/></Menu.Item>
+          
+      </Menu>;
+
+      {/* <Menu theme="dark"   items={[]} /> */}
+
+
+    </Sider>
+    <Layout
+      className="site-layout"
+      style={{
+        marginLeft: 200,
+      }}
+    >
+      <Header
+        className="site-layout-background"
+        style={{
+          padding: 0,
+        }}
+      />
+      <Content
+        style={{
+          margin: '0',
+          overflow: 'initial',
+        }}
+      >
+        <div
+          className="site-layout-background"
+          style={{
+            // padding: 24,
+            // textAlign: 'center',
+          }}
+        >
+           <video ref={videoRef} id="vidContent" width="100%" loop={loop} muted={sound} >
+
+              <source src="http://localhost:888/videos" type="video/mp4"/>
+
+              </video>
+         
+        </div>
+      </Content>
+      
+    </Layout>
+  </Layout>
+
+  )
+
+      }
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,53 @@
+const xpress = require('express')
+const fs = require('fs')
+const app = xpress();
+PORT = 888
+
+
+app.get('/', (req,res) => {
+
+    res.sendFile(__dirname + "/index.html")
+    
+})
+
+app.get('/videos', (req,res) => {
+
+  const range = req.headers.range;
+  console.log(req.headers.range)
+  if (!range) {
+    res.status(400).send("Request must include range header. Please try again.")
+  }
+    const videoPath = "ThirdChild.mp4"
+    // get size of video 
+    const videoSize = fs.statSync(videoPath).size
+    const CHUNK_SIZE = 10 ** 6; //1mb
+    //parse the range 
+    //Example "bytes=8823-"
+    
+    const start = Number(range.replace(/\D/g,"")) //replace all non digit characters with nothing
+    const end = Math.min(start + CHUNK_SIZE, videoSize - 1) //end will continue to increase 
+    console.log('start',start)
+    console.log('end',end)
+
+    const contentLength = end - start + 1
+    const headers = {
+        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "video/mp4"
+    }
+
+    res.writeHead(206,headers); //tells browswer that partial data is being sent
+
+    const videoStream = fs.createReadStream(videoPath, {start,end})
+    videoStream.pipe(res)
+
+    
+})
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on ${PORT}`)
+})
